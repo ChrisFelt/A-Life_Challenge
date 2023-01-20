9dc76faa8637c23607373e5ccb357441608c067f
ChrisFelt <christopher.f.felt@gmail.com>
2023-01-20 01:25:55 -0700

    added some data members and placeholder methods

--- organism.py
+++ organism.py
@@ -1,5 +1,42 @@
+import turtle
+
+
 class Organism:
-    pass
+
+    def __init__(self, position, destination, health, speed, damage, separation_weight, birth_rate, mutation_rate):
+        self._sprite = turtle.Turtle()
+        self._position = position
+        self._destination = destination
+        self._health = health
+        self._speed = speed
+        self._damage = damage
+        self._separation_weight = separation_weight
+        self._birth_rate = birth_rate
+        self._mutation_rate = mutation_rate
+
+    def set_pos(self):
+        pass
+
+    def get_pos(self):
+        pass
+
+    def set_dest(self):
+        pass
+
+    def get_dest(self):
+        pass
+
+    def move(self):
+        """Move towards destination"""
+        pass
+
+    def clear(self):
+        """Remove circle"""
+        pass
+
+    def draw(self):
+        """Draw circle on position"""
+        pass
 
 
 class Predator(Organism):
