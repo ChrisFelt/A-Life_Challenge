15e05a39167a8847c6a36e87ea5a3ce37573ea86
ChrisFelt <christopher.f.felt@gmail.com>
2023-02-06 00:40:43 -0700

    simplified tkinter canvas creation

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ import tkinter
 if __name__ == "__main__":
     organisms = []
     root = tkinter.Tk()
-    canvas = tkinter.Canvas(root, width=settings.screen_size, height=settings.screen_size)
-    # canvas.config(width=settings.frame_width, height=settings.frame_height)
-    canvas.pack(side="top", anchor="nw")
 
-    parameters_window.change_to_parameters(canvas,
-                                          organisms,
-                                          settings.prey_attributes,
-                                          settings.pred_attributes)
-    canvas.mainloop()
+    parameters_window.change_to_parameters(root,
+                                           organisms,
+                                           settings.prey_attributes,
+                                           settings.pred_attributes)
+    root.mainloop()
